e3ab0fe771bbdb4064488fd7f2ccadc1ab86eec3
fahryseptian <fahryseptian35@gmail.com>
2020-12-01 04:18:37 +0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,11 +2,11 @@
  * Link
  */
 export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20210106T080000Z%2F20201003T060000Z&text=Miftah+%26+Fahry+Wedding&details=Miftah+%26+Fahry+Wedding`;
-export const GOOGLE_MAPS_LINK = `https://goo.gl/maps/SYoQCrXBNHN2pfoQ7?share`;
+export const GOOGLE_MAPS_LINK = `https://maps.app.goo.gl/5DieywYhAmkbtn7G6`;
 
 /**
  * Wedding time
 */
-export const EPOCH_START_EVENT = 1613952139;
-export const EPOCH_END_EVENT = 1613952139;
+export const EPOCH_START_EVENT = 1609894800;
+export const EPOCH_END_EVENT = 1609894800;
 export const UTC_WEDDING_TIME = '2021-01-06:00:00:00Z';
