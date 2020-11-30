e667b72545360dd30d7075ccb4c14473be29b478
fahryseptian <fahryseptian35@gmail.com>
2020-11-30 22:19:44 +0700

    Update ButtonLive.js

--- src/components/WeddingSection/ButtonLive.js
+++ src/components/WeddingSection/ButtonLive.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import IconIg from './assets/instagram.png';
+import IconIg from './assets/lokasi.png';
 import { styButtonWrapper } from './styles';
 
 function ButtonLive() {
   return (
     <div className="row">
       <div className="col-md-12">
-        <p className="text__live">Hadir secara virtual melalui siaran langsung instagram: </p>
-        <a href="https://www.instagram.com/dindadipoyono/" target="_blank" rel="noreferrer">
+        <p className="text__live">Hadir secara  langsung : </p>
+        <a href="https://goo.gl/maps/MkTYe5WF9cogx1NY9" target="_blank" rel="noreferrer">
           <div css={styButtonWrapper}>
             <div className="img__wrapper">
-              <img src={IconIg} alt="Live IG" />
+              <img src={IconIg} alt="Lokasi" />
               <span>@dindadipoyono</span>
             </div>
           </div>
