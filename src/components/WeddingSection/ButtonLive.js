d67d4e14be9c14acaa582a7c0b2b9d5688d86fbb
fahryseptian <fahryseptian35@gmail.com>
2020-11-30 22:24:50 +0700

    Update ButtonLive.js

--- src/components/WeddingSection/ButtonLive.js
+++ src/components/WeddingSection/ButtonLive.js
@@ -8,11 +8,11 @@ function ButtonLive() {
     <div className="row">
       <div className="col-md-12">
         <p className="text__live">Hadir secara  langsung : </p>
-        <a href="https://goo.gl/maps/MkTYe5WF9cogx1NY9" target="_blank" rel="noreferrer">
+        <a href="https://goo.gl/maps/gcrhXNAmNhJigVnP6" target="_blank" rel="noreferrer">
           <div css={styButtonWrapper}>
             <div className="img__wrapper">
               <img src={IconIg} alt="Lokasi" />
-              <span>@dindadipoyono</span>
+              <span>Lokasi Acara</span>
             </div>
           </div>
         </a>
