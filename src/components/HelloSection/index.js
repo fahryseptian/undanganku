d8d980afb3fa1769c6374addb203b990ab05b986
fahryseptian <fahryseptian35@gmail.com>
2020-11-30 19:34:23 +0700

    Update index.js

--- src/components/HelloSection/index.js
+++ src/components/HelloSection/index.js
@@ -32,7 +32,7 @@ function HelloSection({ isInvitation }) {
               <div className="desc-groom">
                 <h3 className="main-font">Miftahul Khairat Ritonga, S.Pd.</h3>
                 <p className="parent-name parent-name__top">
-                  Putri Bapak Alm Harris Fadillah Ritonga <br />& Ibu 
+                  Putri Bapak Alm Haris Fadillah Ritonga <br />& Ibu Roslaini pulungan
                 </p>
               </div>
             </div>
@@ -57,24 +57,6 @@ function HelloSection({ isInvitation }) {
         </div>
       </div>
       {!isInvitation ? (
-        <div id="fh5co-couple" className="fh5co-section-gray" css={styWrapper}>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 text-center fh5co-heading">
-                <h2 className="main-font">Seputar Covid-19</h2>
-                <p className="info">
-                  Berkaitan dengan kondisi pandemik saat ini, keberlangsungan acara kami mengacu pada pedoman
-                  Kementerian Kesehatan, yaitu memberlakukan pembatasan jarak dan pembatasan jumlah tamu undangan.
-                </p>
-                <p className="info">
-                  Maka dari itu, dengan segala kerendahan hati, kami mohon maaf jika belum bisa mengundang saudara/i
-                  dihari bahagia kami. Sekiranya do'a dari saudara/i sudah lebih dari cukup bagi kami. Semoga kita semua
-                  selalu dalam lindungan Allah SWT. Aamiin ya rabbal allamiin..
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
       ) : null}
     </Fragment>
   );
