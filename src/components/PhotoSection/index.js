5ae75459233290ad0407abd0d4e0cdc1d8734bf5
fahryseptian <fahryseptian35@gmail.com>
2020-11-30 21:57:05 +0700

    Update index.js

--- src/components/PhotoSection/index.js
+++ src/components/PhotoSection/index.js
@@ -7,10 +7,10 @@ function PhotoSection() {
 const renderYoutubeVideo = () => {
     return (
       <iframe
-        title="Pre-Wedding Dinda & Indra"
+        title="Pre-Wedding Miftah & Fahry"
         width="100%"
         height="360px"
-        src="https://www.youtube.com/embed/75w39OY7N-k"
+        src=""
         frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
