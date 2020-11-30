b025b1b3d4b78887d474df98f79d6a3c09ee9b4c
fahryseptian <fahryseptian35@gmail.com>
2020-11-30 17:35:55 +0700

    Update index.js

--- src/components/LocationSection/index.js
+++ src/components/LocationSection/index.js
@@ -18,18 +18,18 @@ function LocationSection() {
                   rel="noreferrer"
                   style={{ color: '#828282' }}
                 >
-                  <strong>HARRIS Hotel Sentraland Semarang</strong>
+                  <strong>Home</strong>
                 </a>{' '}
                 <br />
-                Jl. Ki Mangunsarkoro No.36, Karangkidul, Kec. Semarang Tengah, <br />
-                Kota Semarang, Jawa Tengah 50136
+               Dusun Batu Lima, Desa T.Tinggi Pangkatan, Kec Pangkatan <br />
+                Kab Labuhan Batu, Sumatera Utara
               </p>
             </div>
           </div>
           <div className="row">
             <div className="col-md-10 col-md-offset-1">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7920.293266961091!2d110.4250066322774!3d-6.992004594949059!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708ca11d0e6f61%3A0x8c359fe27c58195d!2sHARRIS%20Hotel%20Sentraland%20Semarang!5e0!3m2!1sen!2sid!4v1590977401700!5m2!1sen!2sid"
+                src="<iframe src="https://www.google.com/maps/embed?pb=!4v1606731996063!6m8!1m7!1sjXFFReKOiEKXNP0kbUv3XQ!2m2!1d2.089570990229686!2d99.98607633834031!3f0!4f0!5f0.7820865974627469"
                 width="100%"
                 height="450"
                 frameBorder="0"
@@ -37,7 +37,7 @@ function LocationSection() {
                 allowFullScreen
                 aria-hidden="false"
                 tabIndex="0"
-                title="Google Maps - Dinda & Indra Wedding Party"
+                title="Google Maps - Miftah & Fahry Wedding Party"
               ></iframe>
             </div>
           </div>
