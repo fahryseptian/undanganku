27cc49eddf842938f6438538b751c67162be9fe2
fahryseptian <fahryseptian35@gmail.com>
2020-11-30 22:09:35 +0700

    Update ButtonLive.js

--- src/components/WeddingSection/ButtonLive.js
+++ src/components/WeddingSection/ButtonLive.js
@@ -7,12 +7,12 @@ function ButtonLive() {
   return (
     <div className="row">
       <div className="col-md-12">
-        <p className="text__live">Hadir secara langsung : </p>
-        <a href="https://calendar.google.com/calendar/u/0/r/eventedit?dates=20210106T080000Z/20201003T060000Z&text=Miftah+%26+Fahry+Wedding&details=Miftah+%26+Fahry+Wedding&pli=1&sf=true" target="_blank" rel="noreferrer">
+        <p className="text__live">Hadir secara virtual melalui siaran langsung instagram: </p>
+        <a href="https://www.instagram.com/dindadipoyono/" target="_blank" rel="noreferrer">
           <div css={styButtonWrapper}>
             <div className="img__wrapper">
-              <img src={IconCalender} alt="simpan di kalender" />
-              
+              <img src={IconIg} alt="Live IG" />
+              <span>@dindadipoyono</span>
             </div>
           </div>
         </a>
