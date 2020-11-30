0ca2b8c1fc0b712e5de92ea8e73651e0346e1aeb
fahryseptian <fahryseptian35@gmail.com>
2020-11-30 23:00:49 +0700

    Update stories-data.js

--- src/components/StorySection/stories-data.js
+++ src/components/StorySection/stories-data.js
@@ -7,44 +7,38 @@ import ImgNdodokLawang from '@assets/images/p-ndodok-lawang.jpg';
 export const stories = [
   {
     title: 'Jumpa Pertama ',
-    date: '10 Oktober 2015',
+    date: '2010 - 2013',
     description:
-      'Pertama kali kami dipertemukan di sebuah acara malam keakraban di kampus Universitas Dian Nuswantoro. Sebagai seorang mahasiswa baru, kala itu Dinda melihat Indra sedang sibuk memegang megaphone (toa) di tengah hiruk pikuk acara. Belum ada sesuatu yang spesial, kala itu justru Dinda melihat Indra banyak menerima surat pink (tanda kagum) dari teman sesama mahasiswa barunya.',
+      'Pertama kali kami dipertemukan di sebuah Pondok Pesantren Modren Daar Al Uluum Kisaran. Sebagai seorang Santri dan Santriwati, kala itu Fahry sering perhatian ke Miftah sebagai tanda kagum :D.',
     image: '',
   },
   {
     title: 'Kembali Berjumpa',
-    date: '9 September 2017',
+    date: '22 Mei 2016',
     description:
-      'Konferensi Future Leader Summit 2017 kembali mempertemukan mereka. Dinda sebagai peserta dan Indra sebagai panitia acara. Mulai dari saling sapa, mereka berdua tumbuh perasaan saling ingin mengenal lebih dalam. Seusai acara, Indra memberanikan diri untuk membangun komunikasi dengan Dinda. Dari pesan singkat pertama yang dikirimnya, Indra dan Dinda mulai dekat dan menjadi lebih sering mengirim kabar. ',
-    image: ImgKembaliBerjumpa,
+      'Reuni Santri di Medan kembali mempertemukan mereka. Mulai dari saling sapa, mereka berdua tumbuh perasaan saling ingin mengenal lebih dalam. Seusai acara, Fahry memberanikan diri untuk mengungkapkan perasaan lamanya. dan hari itu juga kami jadian  ',
+    image: ,
   },
   {
-    title: 'Kisah Kasih di Kampus',
-    date: 'Oktober 2017 - 2018',
+    title: 'Pejuang Sarjana',
+    date: 'Oktober 2015 - 2019',
     description:
-      'Sebagai sepasang mahasiswa dan mahasiswi, Dinda dan Indra memilih untuk mengisi kisah kasih di kampusnya dengan mengikuti berbagai kompetisi bersama. Kala itu, Dinda adalah mahasiswi Ilmu Komunikasi dan Indra adalah mahasiswa Teknik Informatika. Merupakan kolaborasi yang pas jika mengikuti kompetisi teknologi, Indra berperan sebagai seorang programmer dan Dinda sebagai seorang content development. Dinda dan Indra pernah membuat aplikasi Ayo Toba (Aplikasi Pariwisata Danau Toba) yang diajukan dalam kompetisi IT FEST Universitas Sumatera Utara di awal tahun 2018.',
-    image: ImgJakartaKroasia,
+      'Sebagai sepasang mahasiswa dan mahasiswi, Miftah dan Fahry memilih untuk mengisi kisah kasih dengan mendukung perkuliahan dan tugas tugas kampus. Kala itu, Miftah adalah mahasiswi Tata Rias dan Fahry adalah mahasiswa Teknik Informatika. Merupakan kolaborasi yang pas jika membangun startup, dan  pernah membuat Website Medanrias.com yang di ajukan ke kompetisi IDCAMP Indosat Ooredoo menjadi pemenang terbaik.',
+    image: ,
   },
   {
-    title: 'Jakarta & Kroasia',
-    date: '2018 - 2019',
+    title: 'Medan & Aek Nabara',
+    date: '2019 - 2020',
     description:
-      'Tidak selalu bersama, perjuangan dimulai ketika Indra memutuskan untuk bekerja di Jakarta sebagai seorang Software Engineer di Tokopedia. Belum usai masa LDR, pada 3 Oktober 2018, Dinda juga memutuskan untuk mengambil beasiswa di University of Split, Kroasia. Di bulan November 2018, Indra melangsungkan wisuda dan Dinda hanya bisa memberi virtual support melalui video call. ',
-    image: ImgBoarding,
+      'Tidak selalu bersama, perjuangan dimulai ketika Fahry memutuskan untuk bekerja di Medan sebagai seorang Network Operation Center di PT Netciti Persada. Miftah juga memutuskan untuk Stay dahulu di Kampung halaman untuk menemani keluarga di rumah. ',
+    image: ,
   },
+  
   {
-    title: 'Kembali ke Indonesia',
-    date: '3 Maret 2019',
+    title: 'Lamaran',
+    date: '08 November 2020',
     description:
-      'Periode belajar di Kroasia telah selesai. Akhirnya Dinda kembali ke Indonesia! Ada tanggung jawab yang harus Dinda selesaikan seusai dari Kroasia, yakni harus menyelesaikan skripsi dan mencapai target untuk wisuda di bulan Agustus 2019. Selain itu, kembalinya Dinda ke Indonesia menjadi momen yang tepat bagi Indra untuk menyampaikan niat ingin serius kepada orang tua Dinda. Hal itu disambut baik oleh kedua orang tua mereka  dan sepakat akan melaksanakan acara Ndodok Lawang usai Dinda wisuda. Sebelum wisuda, Dinda sudah harus meniti karir di Jakarta sebagai seorang Content Analyst di Ruangguru.',
-    image: ImgKembaliKeIndonesia,
-  },
-  {
-    title: 'Ndodok Lawang',
-    date: '31 Agustus 2019',
-    description:
-      'Akhirnya tiba di masa Indra dan keluarga hadir ke rumah Dinda untuk menyampaikan niat baik. Mulai dari acara Ndodok Lawang ini, dilanjutkan dengan persiapan pernikahan yang insha Allah akan diselenggarakan pada tanggal 3 Oktober 2020.',
-    image: ImgNdodokLawang,
+      'Akhirnya tiba di masa Fahry dan keluarga hadir ke rumah Miftah untuk menyampaikan niat baik.  Untuk persiapan pernikahan yang insha Allah akan diselenggarakan pada tanggal 6 Januari 2021.',
+    image: ,
   },
 ];
