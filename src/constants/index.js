e981a14aee34adcdd02fefd9103d0914dea3e190
fahryseptian <fahryseptian35@gmail.com>
2020-11-30 17:42:19 +0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,12 @@
 /**
  * Link
  */
-export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20201003T040000Z%2F20201003T060000Z&text=Dinda+%26+Indra+Wedding&details=Dinda+%26+Indra+Wedding`;
-export const GOOGLE_MAPS_LINK = `https://g.page/HARRISHotelSentralandSemarang?share`;
+export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20210106T080000Z%2F20201003T060000Z&text=Miftah+%26+Fahry+Wedding&details=Miftah+%26+Fahry+Wedding`;
+export const GOOGLE_MAPS_LINK = `https://goo.gl/maps/SYoQCrXBNHN2pfoQ7?share`;
 
 /**
  * Wedding time
  */
 export const EPOCH_START_EVENT = 1601686800;
 export const EPOCH_END_EVENT = 1601704800;
-export const UTC_WEDDING_TIME = '2020-10-03:04:00:00Z';
+export const UTC_WEDDING_TIME = '2021-01-06:08:00:00Z';
