18358711183ad2a9ac171a8b668ad10e2931d9fb
fahryseptian <fahryseptian35@gmail.com>
2020-12-01 16:49:58 +0700

    Update photo-data.js

--- src/components/PhotoSection/photo-data.js
+++ src/components/PhotoSection/photo-data.js
@@ -1,9 +1,6 @@
 import Gallery1 from '@assets/images/gallery-1.jpg';
-import GalleryT1 from '@assets/images/gallery-t-1.jpg';
 import Gallery2 from '@assets/images/gallery-2.jpg';
-import GalleryT2 from '@assets/images/gallery-t-2.jpg';
 import Gallery3 from '@assets/images/gallery-3.jpg';
-import GalleryT3 from '@assets/images/gallery-t-3.jpg';
 import Image5 from '@assets/images/slide-5.jpg';
 import Image5thumb from '@assets/images/slide-5-thumb.jpg';
 import Image6 from '@assets/images/slide-6.jpg';
@@ -13,7 +10,7 @@ import Image6thumb from '@assets/images/slide-6-thumb.jpg';
 export const photos = [
   {
     original: Gallery1,
-    thumbnail: GalleryT1,
+    
   },
   {
     original: Image6,
@@ -25,11 +22,11 @@ export const photos = [
   },
   {
     original: Gallery2,
-    thumbnail: GalleryT2,
+    
   },
   {
     original: Gallery3,
-    thumbnail: GalleryT3,
+    
   },
   
 ];
