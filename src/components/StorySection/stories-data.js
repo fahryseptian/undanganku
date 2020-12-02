014024c890e41378734b52f310728e110d36c855
fahryseptian <fahryseptian35@gmail.com>
2020-12-02 08:20:32 +0700

    Update stories-data.js

--- src/components/StorySection/stories-data.js
+++ src/components/StorySection/stories-data.js
@@ -1,8 +1,7 @@
-import ImgKembaliBerjumpa from '@assets/images/p-kembali-berjumpa.jpg';
-import ImgJakartaKroasia from '@assets/images/p-jakarta-kroasia.jpg';
-import ImgBoarding from '@assets/images/p-boarding-smg.jpg';
-import ImgKembaliKeIndonesia from '@assets/images/p-kembali-ke-indonesia.jpg';
-import ImgNdodokLawang from '@assets/images/p-ndodok-lawang.jpg';
+import ldr from '@assets/images/ldr.jpg';
+import sidang from '@assets/images/sidang.jpg';
+import jumpakembali from '@assets/images/jumpakembali.jpg';
+import lamaran from '@assets/images/lamaran.jpg';
 
 export const stories = [
   {
@@ -17,21 +16,21 @@ export const stories = [
     date: '22 Mei 2016',
     description:
       'Reuni Santri di Medan kembali mempertemukan mereka. Mulai dari saling sapa, mereka berdua tumbuh perasaan saling ingin mengenal lebih dalam. Seusai acara, Fahry memberanikan diri untuk mengungkapkan perasaan lamanya. dan hari itu juga kami jadian  ',
-    image:'' ,
+    image:jumpakembali ,
   },
   {
     title: 'Pejuang Sarjana',
     date: 'Oktober 2015 - 2019',
     description:
       'Sebagai sepasang mahasiswa dan mahasiswi, Miftah dan Fahry memilih untuk mengisi kisah kasih dengan mendukung perkuliahan dan tugas tugas kampus. Kala itu, Miftah adalah mahasiswi Tata Rias dan Fahry adalah mahasiswa Teknik Informatika. Merupakan kolaborasi yang pas jika membangun startup, dan  pernah membuat Website Medanrias.com yang di ajukan ke kompetisi IDCAMP Indosat Ooredoo menjadi pemenang terbaik.',
-    image: '',
+    image: sidang,
   },
   {
     title: 'Medan & Aek Nabara',
     date: '2019 - 2020',
     description:
       'Tidak selalu bersama, perjuangan dimulai ketika Fahry memutuskan untuk bekerja di Medan sebagai seorang Network Operation Center di PT Netciti Persada. Miftah juga memutuskan untuk Stay dahulu di Kampung halaman untuk menemani keluarga di rumah. ',
-    image:'' ,
+    image:ldr,
   },
   
   {
@@ -39,6 +38,6 @@ export const stories = [
     date: '08 November 2020',
     description:
       'Akhirnya tiba di masa Fahry dan keluarga hadir ke rumah Miftah untuk menyampaikan niat baik.  Untuk persiapan pernikahan yang insha Allah akan diselenggarakan pada tanggal 6 Januari 2021.',
-    image:'' ,
+    image:lamaran ,
   },
 ];
