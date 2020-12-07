120bd94a966156071faeb8e04e8566d1f5501a9e
fahryseptian <fahryseptian35@gmail.com>
2020-12-07 18:00:37 +0700

    Update wishlist-data.js

--- src/components/WishesSection/wishlist-data.js
+++ src/components/WishesSection/wishlist-data.js
@@ -1,4 +1,5 @@
 import Dinda from '@assets/images/w-dinda.jpg';
+import Pu from '@assets/images/pu.jpg';
 
 export const wishlist = [
   {
@@ -6,6 +7,12 @@ export const wishlist = [
     infoName: 'Adik Kandung Fahry',
     description: `selamat menempuh hidup baru Kak Miftah dan Bang Fahry semoga menjadi pasangan teman hidup selamanya sesurga, enjoy your little family always be happy my sweet couple!🥰🤗 amin`,
     image: Dinda,
+  },
+  {
+    name: 'Teman Kuliah',
+    infoName: 'Temen Seperjuangan Di Potensi Utama',
+    description: `Selamat lah yaa cekk ,semoga tersemogakan semua lah yaa cekkk 😁😊 aamin`,
+    image: Pu,
   },
   
 ];
